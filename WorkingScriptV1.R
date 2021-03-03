e620f993ce0461d30a8eb1cc8cb326ad3f830e76
Tyler Washburn <tjw77015@uga.edu>
2021-03-03 12:34:45 -0500

    Checked NA of JClass

--- WorkingScriptV1.R
+++ WorkingScriptV1.R
@@ -6,3 +6,5 @@ JClass <- read.csv('JournalsClassification.csv', header = T)
 head (JClass)
 
 # Tyler - load in main spreadsheet after you've data cleaned and see if it will read in ok
+
+sum(is.na(JClass))
