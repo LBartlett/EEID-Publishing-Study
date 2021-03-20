a1fe7403e92f30efd9a14a8bccd4d4ae88a5fd0d
Lewis J Bartlett <lewis.bartlett@uga.edu>
2021-03-20 13:05:35 -0400

    Part 1 ESA data frame making

--- WorkingScriptV1.R
+++ WorkingScriptV1.R
@@ -4,7 +4,33 @@
 
 JClass <- read.csv('JournalsClassification.csv', header = T)
 head (JClass)
-
-# Tyler - load in main spreadsheet after you've data cleaned and see if it will read in ok
-
 sum(is.na(JClass))
+
+DataRaw <- read.csv('MainDataESA2017.csv', header = T)
+head(DataRaw)
+
+# Assemble new data frame for analysis
+
+ESA2017 <- data.frame('Talk' = 1:NROW(DataRaw))
+
+for (N in 1:NROW(ESA2017)){
+  
+  ESA2017$EEIDSession[N] <- DataRaw$EEIDSession[N]
+  
+  ESA2017$Located[N] <- DataRaw$PublicationFound[N]
+  
+}
+
+ESA2017 <- na.omit(ESA2017)
+
+for (N in ESA2017$Talk){
+  
+  for (N in 1:NROW(ESA2017)){
+    
+    ESA2017$Year[N] <- DataRaw$Year[N]
+    
+    ESA2017$Located[N] <- DataRaw$PublicationFound[N]
+    
+  }
+  
+}
