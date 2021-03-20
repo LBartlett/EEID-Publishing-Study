b2578b8f836e863e9ec5261c5892527616c11334
Lewis J Bartlett <lewis.bartlett@uga.edu>
2021-03-20 19:43:08 -0400

    Assembled data frame for next steps of work

--- WorkingScriptV1.R
+++ WorkingScriptV1.R
@@ -9,7 +9,7 @@ sum(is.na(JClass))
 DataRaw <- read.csv('MainDataESA2017.csv', header = T)
 head(DataRaw)
 
-# Assemble new data frame for analysis
+# Assemble a new data frame for analysis
 
 ESA2017 <- data.frame('Talk' = 1:NROW(DataRaw))
 
@@ -23,14 +23,36 @@ for (N in 1:NROW(ESA2017)){
 
 ESA2017 <- na.omit(ESA2017)
 
-for (N in ESA2017$Talk){
+for (N in 1:NROW(ESA2017)){
   
-  for (N in 1:NROW(ESA2017)){
+  n <- ESA2017$Talk[N]
+  
+  ESA2017$Year[N] <- DataRaw$Year[n]
+  
+  ESA2017$Journal[N] <- DataRaw$Journal[n]
+  
+  if(is.na(ESA2017$Journal[N])){
     
-    ESA2017$Year[N] <- DataRaw$Year[N]
+    ESA2017$GenBio[N] <- NA
+    ESA2017$EEIDSpec[N] <- NA
+    ESA2017$EcoSpec[N] <- NA
     
-    ESA2017$Located[N] <- DataRaw$PublicationFound[N]
+  }else{
+    
+    ESA2017$GenBio[N] <- JClass$GeneralBiology[which(JClass$Journal == ESA2017$Journal[N])]
+    ESA2017$EEIDSpec[N] <- JClass$EEIDSpecific[which(JClass$Journal == ESA2017$Journal[N])]
+    ESA2017$EcoSpec[N] <- JClass$EcologySpecific[which(JClass$Journal == ESA2017$Journal[N])]
     
   }
-  
 }
+
+# Steps for Tyler:
+# see if you can figure out roughly how the above works
+# think about what the data types are, and how one section of the data will need 'relabelling'
+# relabel certain parts of the data as appropriate
+# have a go at some form of statistical analysis
+
+
+
+
+
