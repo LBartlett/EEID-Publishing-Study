cfb58a3bd85288fef2c70b39d35ba038bd6d2dbb
Lewis J Bartlett <lewis.bartlett@uga.edu>
2021-04-01 13:44:02 -0400

    Fixed convert to logical
    Ran first binomial GLM (EcoSpec~EEIDSesh)

--- WorkingScriptV1.R
+++ WorkingScriptV1.R
@@ -52,10 +52,33 @@ for (N in 1:NROW(ESA2017)){
 # relabel certain parts of the data as appropriate
 # have a go at some form of statistical analysis
 
-  ESA2017[ESA2017 == "N"] <- FALSE
-  ESA2017[ESA2017 == "Y"] <- TRUE
- GenbioTotal <- sum(ESA2017$GenBio == TRUE, na.rm = TRUE)
- EEIDSpecTotal <- sum(ESA2017$EEIDSpec == TRUE, na.rm = TRUE)
- EcoSpecTotal <- sum(ESA2017$EcoSpec == TRUE, na.rm = TRUE)
- if (ESA2017$EEIDSession == "YES") GenBioEEID <- {sum(ESA2017$GenBio == TRUE, na.rm = TRUE)}
- 
+ESA2017[ESA2017 == "N"] <- FALSE
+ESA2017[ESA2017 == "Y"] <- TRUE
+
+ESA2017$GenBio <- as.logical(ESA2017$GenBio)
+ESA2017$EEIDSpec <- as.logical(ESA2017$EEIDSpec)
+ESA2017$EcoSpec <- as.logical(ESA2017$EcoSpec)
+
+GenbioTotal <- sum(ESA2017$GenBio, na.rm = TRUE)
+EEIDSpecTotal <- sum(ESA2017$EEIDSpec, na.rm = TRUE)
+EcoSpecTotal <- sum(ESA2017$EcoSpec, na.rm = TRUE)
+
+GenBioEEID <- sum(ESA2017$GenBio[which(ESA2017$EEIDSession == "YES")], na.rm = TRUE)
+EEIDSpecEEID <- sum(ESA2017$EEIDSpec[which(ESA2017$EEIDSession == "YES")], na.rm = TRUE)
+EcoSpecEEID <- sum(ESA2017$EcoSpec[which(ESA2017$EEIDSession == "YES")], na.rm = TRUE)
+
+
+# Run a binomial GLM looking at whether being published in an ecology-specific journal is predicted in part by being presented at an EEID session or not.
+EcoModel1 <- glm(formula = EcoSpec ~ EEIDSession,
+                 family = 'binomial',
+                 data = na.omit(ESA2017)
+)
+# Look at a summary of the model outputs: most important here is the 'estimate' values of the 'coefficient' section
+# (that tells us effect size and direction)
+summary(EcoModel1)
+
+# get the p-value from a Chi-Squared anova (don't look at the p-values in the summary :) )
+anova(EcoModel1, test = 'Chisq')
+
+
+
